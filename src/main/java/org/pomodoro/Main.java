4e6042e8714677c0fc3ccf51a17ce0d7499d6e88
Jihoon Park <chad9349@gmail.com>
2023-09-17 20:30:17 +0900

    process console input

--- src/main/java/org/pomodoro/Main.java
+++ src/main/java/org/pomodoro/Main.java
@@ -4,23 +4,29 @@ import org.pomodoro.console.render.ConsoleRenderer;
 import org.pomodoro.core.PomodoroTimer;
 import org.pomodoro.core.PomodoroTimerFactory;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         ConsoleRenderer renderer = new ConsoleRenderer();
+        Scanner scanner = new Scanner(System.in);
         PomodoroTimerFactory factory = PomodoroTimerFactory.getInstance();
 
         renderer.clearConsole();
         renderer.printMenu();
 
-        renderer.clearConsole();
-        PomodoroTimer pomodoroTimer = factory.next();
-        pomodoroTimer.start();
-        while (!pomodoroTimer.isFinished()) {
+        String command = scanner.next();
+        if (command.equals("1")) {
+            renderer.clearConsole();
+            PomodoroTimer pomodoroTimer = factory.next();
+            pomodoroTimer.start();
+            while (!pomodoroTimer.isFinished()) {
+                renderer.printProgressBar(pomodoroTimer);
+                Thread.sleep(100);
+            }
             renderer.printProgressBar(pomodoroTimer);
-            Thread.sleep(100);
+            renderer.printFinishMessage();
         }
-        renderer.printProgressBar(pomodoroTimer);
-        renderer.printFinishMessage();
 
         renderer.printQuit();
     }
